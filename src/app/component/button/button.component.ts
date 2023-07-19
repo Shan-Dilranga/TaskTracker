5806b870b2b621d3c0ac767ba77dbcb087b473ed
Shan-Dilranga <2019cs187@stu.ucsc.lk>
2023-07-19 23:40:49 +0530

    built a button component as a reusable one

--- src/app/component/button/button.component.ts
+++ src/app/component/button/button.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.css']
+  
 })
-export class ButtonComponent {
+export class ButtonComponent implements OnInit{
 
+  
+  
+  @Input() text:string | undefined;
+  
+  @Output() btnclick= new EventEmitter();
+
+  constructor(){}
+
+  ngOnInit(): void {
+    
+  }
+
+  onclick(){
+    this.btnclick.emit();
+  }
 }
